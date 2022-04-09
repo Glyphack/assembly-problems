39d68b8cce2a9c6cd0541a5f766cbb98b83b8b17
Shayegan Hooshyari <shayegan.hooshyari@carnext.com>
2022-04-09 16:05:06 +0430

    Add sum solution

--- sum.asm
+++ sum.asm
@@ -1,18 +1,20 @@
+%include "in_out.asm"
+
 section .data
 
 section .bss
-	num:	resb 4
-	num2:	resb 4
+	num:	resb 8
+	num2:	resb 8
 
 section .text
 	global	_start
 
 _start:
 	call	readNum
-	mov	[num], rax
+	mov		[num], rax
 	call	readNum
-	mov	[num2], rax
-	add	rax, [num2]
+	mov		[num2], rax
+	add		rax, [num]
 	call	writeNum
 
 
